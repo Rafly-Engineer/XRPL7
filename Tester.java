793108ec7578f68eb25c37ce9524c1ae77629cf7
Rafly-Engineer <muhammad_rachman_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 00:22:27 -0700

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,62 @@
+//Driver Class
+public class Tester {
+    public static void main(String[] args) {
+        //object
+        //Class object = new Constructor
+        Siswa burhan = new Siswa();
+        Siswa ibra = new Siswa();
+        Siswa gilam = new Siswa();
+        Siswa moreno = new Siswa();
+        Siswa azam = new Siswa();
+        
+        burhan.id = 26;
+        burhan.nama = "Burhanudin";
+        burhan.ipk = 90;
+
+        ibra.id = 29;
+        ibra.nama = "nibras";
+        ibra.ipk = 90;
+
+        gilam.id = 19;
+        gilam.nama = "gilam";
+        gilam.ipk = 90;
+
+        moreno.id = 24;
+        moreno.nama = "moreno";
+        moreno.ipk = 90;
+
+        azam.id = 22;
+        azam.nama = "azam";
+        azam.ipk = 90;
+
+
+        System.out.println("id: "+burhan.id);
+        System.out.println("nama: "+burhan.nama);
+        System.out.println("ipk: "+burhan.ipk);
+        System.out.println("   ");
+        
+        System.out.println("id: "+ibra.id);
+        System.out.println("nama: "+ibra.nama);
+        System.out.println("ipk: "+ibra.ipk);
+        System.out.println("   ");
+
+        System.out.println("id: "+gilam.id);
+        System.out.println("nama: "+gilam.nama);
+        System.out.println("ipk: "+gilam.ipk);
+        System.out.println("   ");
+
+        System.out.println("id: "+moreno.id);
+        System.out.println("nama: "+moreno.nama);
+        System.out.println("ipk: "+moreno.ipk);
+        System.out.println("   ");
+
+        System.out.println("id: "+azam.id);
+        System.out.println("nama: "+azam.nama);
+        System.out.println("ipk: "+azam.ipk);
+        System.out.println("   ");
+
+
+
+        
+    }
+}
