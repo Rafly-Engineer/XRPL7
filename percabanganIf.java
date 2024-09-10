3102b89dd3cb8076ebf312848256e35bdd2dd932
Rafly-Engineer <muhammad_rachman_33rpl@student.smktelkom-mlg.sch.id>
2024-09-10 01:16:29 -0700

    Add files via upload

---
+++ percabanganIf.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class percabanganIf {
+    public static void main(String[] args) {
+        double total;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Masukkan Total: ");
+        total = s.nextDouble();
+        if (total >100000) {
+            System.out.println("Anda Mendapatkan Diskon 10% ");
+            
+        }
+
+
+
+        
+    }
+}
